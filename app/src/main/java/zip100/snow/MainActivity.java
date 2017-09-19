442620872193f75cc54986a94e9412a83b40c8f1
mike <memcache@qq.com>
2017-09-19 23:05:37 +0800

    connect activify

--- app/src/main/java/zip100/snow/MainActivity.java
+++ app/src/main/java/zip100/snow/MainActivity.java
@@ -1,13 +1,26 @@
 package zip100.snow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_connect).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        if(view.getId() == R.id.button_connect){
+            Intent intent = new Intent();
+            intent.setClass(this,ConnectActivity.class);
+            startActivity(intent);
+        }
     }
 }
