7279bfe8cade87a64765d8ad103ecf82b08ac6d4
mike <memcache@qq.com>
2017-09-20 19:28:07 +0800

    增加 socket

--- app/src/main/java/zip100/snow/ConnectActivity.java
+++ app/src/main/java/zip100/snow/ConnectActivity.java
@@ -1,13 +1,118 @@
 package zip100.snow;
 
+import android.os.Handler;
+import android.os.Message;
+import android.os.StrictMode;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ConnectException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+import java.util.Timer;
+import java.util.logging.LogRecord;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class ConnectActivity extends AppCompatActivity {
+
+    private static String IpAddress = "172.19.21.128";
+    private static int Port = 81;
+    Socket socket = null;
+
+    public static final int EXTCEPTION = 1;
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == EXTCEPTION) {
+                Bundle data = msg.getData();
+                AlertDialog.Builder builder = new AlertDialog.Builder(ConnectActivity.this);
+                builder.setMessage(data.getString("msg"));
+                builder.setTitle("提示");
+                builder.show();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
+
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectDiskReads().detectDiskWrites().detectNetwork().penaltyLog().build());
+
+
+        findViewById(R.id.button_connect).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            // 创建socket对象，指定服务器端地址和端口号
+                            socket = new Socket(IpAddress, Port);
+                            // 填充信息
+                            Log.d("Socket", "msg=");
+
+
+                            // 获取 Client 端的输出流
+                            PrintWriter out = new PrintWriter(new BufferedWriter(
+                                    new OutputStreamWriter(socket.getOutputStream())), true);
+
+                            while (true) {
+                                Log.d("erro", "loop");
+                                try {
+                                    Thread.sleep(500);
+
+                                    // 填充信息
+                                    //out.println("guoi ahhaah");
+                                    out.write("ahhaah");
+                                    out.flush();
+                                } catch (InterruptedException e) {
+                                    Log.d("erro", e.getMessage());
+                                    // 关闭
+                                    socket.close();
+                                }
+                            }
+
+                        } catch (UnknownHostException e1) {
+                            e1.printStackTrace();
+                        } catch (ConnectException e) {
+                            Message msg = new Message();
+                            msg.what = EXTCEPTION;
+
+                            Bundle data = new Bundle();
+                            data.putString("msg", e.getMessage());
+
+                            msg.setData(data);
+
+                            handler.sendMessage(msg);
+
+                        } catch (IOException e1) {
+                            e1.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        });
     }
 }
--- app/src/main/java/zip100/snow/MainActivity.java
+++ app/src/main/java/zip100/snow/MainActivity.java
@@ -3,9 +3,17 @@ package zip100.snow;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
+
+    float x1 = 0;
+    float x2 = 0;
+    float y1 = 0;
+    float y2 = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,14 +21,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.button_connect).setOnClickListener(this);
+        findViewById(R.id.activify_main).setOnTouchListener(this);
     }
 
     @Override
-    public void onClick(View view){
-        if(view.getId() == R.id.button_connect){
+    public void onClick(View view) {
+        if (view.getId() == R.id.button_connect) {
             Intent intent = new Intent();
-            intent.setClass(this,ConnectActivity.class);
+            intent.setClass(this, ConnectActivity.class);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public boolean onTouch(View var1, MotionEvent event) {
+
+
+        Log.d("touch", "X:" + String.valueOf(event.getX()) + "  Y:" + String.valueOf(event.getY()));
+
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            x1 = event.getX();
+            y1 = event.getY();
+        }
+
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            x2 = event.getX();
+            y2 = event.getY();
+            if (y1 - y2 > 50) {
+                Log.d("touch", "向上滑" + String.valueOf(y1 - y2));
+            } else if (y2 - y1 > 50) {
+                Log.d("touch", "向下滑" + String.valueOf(y2 - y1));
+            } else if (x1 - x2 > 50) {
+                Log.d("touch", "向左滑" + String.valueOf(x1 - x2));
+            } else if (x2 - x1 > 50) {
+                Log.d("touch", "向右滑" + String.valueOf(x2 - x1));
+            }
+        }
+
+        return true;
     }
 }
